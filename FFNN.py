bc9290e2a890e369de306b60ff53c1e45323b444
Andrew <a.choi11@hotmail.com>
2021-02-28 18:25:54 -0700

    creating outline of my feed forward neural network model

---
+++ FFNN.py
@@ -0,0 +1,29 @@
+""" General outline of my Feed Forward Neural Network"""
+""" will end up being a logistic regression model"""
+""" may implement my own model from scratch rather than using pytorch"""
+""" using this as a guideline as I have experience with pytorch"""
+
+import torch
+import torch.nn as nn
+
+
+# Import data
+""" being done by another group member"""
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# number of words/features
+n_features = 500
+n_classes = 8
+
+class ffNN(nn.Module):
+    def __init__(self):
+        super(ffNN, self).__init__()
+
+        # will add more layers
+        self.l1 = nn.linear(n_features, n_classes)
+
+    def forward(self, vector):
+        pred = self.l1(vector)
+
+
